6d920eb3593a9f42d1e21be4cdb1b6af9368cbef
Habib-Ahmad <ahmadalkali.dev@gmail.com>
2023-01-02 13:31:03 +0100

    Initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <p>Hello world!</p>
+    </div>
+  );
+};
+
+export default Home;
