d34b492674f997e1c524ca02d4cddacbba57a8ad
Ahmad Habib <ahmadalkali69@gmail.com>
2023-01-10 21:38:20 +0100

    Created contact page

---
+++ src/components/ContactHeader/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.scss";
+
+const ContactHeader = () => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Contact us</h1>
+        <p>Get in touch</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactHeader;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,11 @@
+import ContactHeader from "../components/ContactHeader";
+
+const Contact = () => {
+  return (
+    <>
+      <ContactHeader />
+    </>
+  );
+};
+
+export default Contact;
