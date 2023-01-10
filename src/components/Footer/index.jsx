a1812f377f4c69d9278f566fd9800a4ee4e1d528
Ahmad Habib <ahmadalkali69@gmail.com>
2023-01-10 21:32:38 +0100

    Added copyeight section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,56 +5,62 @@ import styles from "./styles.module.scss";
 
 const Footer = () => {
   return (
-    <footer className={styles.container}>
-      <Image src={logo} alt="taiseer zarruk" className={styles.logo} />
+    <>
+      <footer className={styles.container}>
+        <Image src={logo} alt="taiseer zarruk" className={styles.logo} />
 
-      <div className={styles.col}>
-        <p className={styles.title}>Menu</p>
+        <div className={styles.col}>
+          <p className={styles.title}>Menu</p>
 
-        <div className={styles.items}>
-          {menuItems.map((item, index) => (
-            <Link key={index} href={item.to}>
-              {item.name}
+          <div className={styles.items}>
+            {menuItems.map((item, index) => (
+              <Link key={index} href={item.to}>
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <div className={`${styles.col} ${styles.follow}`}>
+          <p className={styles.title}>Follow</p>
+
+          <div className={styles.items}>
+            {followItems.map((item, index) => (
+              <Link key={index} href={item.to}>
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <div className={`${styles.col} ${styles.contact}`}>
+          <p className={styles.title}>Contact</p>
+
+          <div className={styles.details}>
+            <Link
+              className={styles.detail}
+              href="https://goo.gl/maps/X3MiPf9JTB9XtsHF9"
+            >
+              <span>Address:</span> Elevators and Escalators Nigeria Limited
+              Mabushi Abuja Nigeria.
             </Link>
-          ))}
-        </div>
-      </div>
-
-      <div className={`${styles.col} ${styles.follow}`}>
-        <p className={styles.title}>Follow</p>
-
-        <div className={styles.items}>
-          {followItems.map((item, index) => (
-            <Link key={index} href={item.to}>
-              {item.name}
+            <Link className={styles.detail} href="tel:+2348064402562">
+              <span>Phone:</span> +2348064402562
             </Link>
-          ))}
+            <Link
+              className={styles.detail}
+              href="mailTo:info@taiseerzarrukltd.com"
+            >
+              <span>Email:</span> info@taiseerzarrukltd.com
+            </Link>
+          </div>
         </div>
-      </div>
+      </footer>
 
-      <div className={`${styles.col} ${styles.contact}`}>
-        <p className={styles.title}>Contact</p>
-
-        <div className={styles.details}>
-          <Link
-            className={styles.detail}
-            href="https://goo.gl/maps/X3MiPf9JTB9XtsHF9"
-          >
-            <span>Address:</span> Elevators and Escalators Nigeria Limited
-            Mabushi Abuja Nigeria.
-          </Link>
-          <Link className={styles.detail} href="tel:+2348064402562">
-            <span>Phone:</span> +2348064402562
-          </Link>
-          <Link
-            className={styles.detail}
-            href="mailTo:info@taiseerzarrukltd.com"
-          >
-            <span>Email:</span> info@taiseerzarrukltd.com
-          </Link>
-        </div>
-      </div>
-    </footer>
+      <p className={styles.copyright}>
+        © 2022 Taiseerzarrukltd / All Rights Reserved.
+      </p>
+    </>
   );
 };
 
